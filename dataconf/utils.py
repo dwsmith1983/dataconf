9bdf62f5ae060348251dbbb5c9c74217a5256d0f
Dustin Smith <Dustin.William.Smith@gmail.com>
2021-09-22 18:12:53 +0700

    updated gitignore for poetry.lock and git 2 (not sure where this came from); added deprecation for loads and load; updated toml for deprecation

--- dataconf/utils.py
+++ dataconf/utils.py
@@ -1,7 +1,6 @@
 from dataclasses import asdict
 from dataclasses import fields
 from dataclasses import is_dataclass
-import deprecation
 from typing import get_args
 from typing import get_origin
 from typing import Union
@@ -10,14 +9,15 @@ from dataconf.exceptions import MalformedConfigException
 from dataconf.exceptions import MissingTypeException
 from dataconf.exceptions import TypeConfigException
 from dataconf.exceptions import UnexpectedKeysException
+from dataconf.version import __version__
 from dateutil.relativedelta import relativedelta
+import deprecation
 from pyhocon import ConfigFactory
 from pyhocon import HOCONConverter
 from pyhocon.config_tree import ConfigList
 from pyhocon.config_tree import ConfigTree
 from pyhocon.exceptions import ConfigMissingException
 import pyparsing
-from dataconf.version import __version__
 
 NoneType = type(None)
 
@@ -159,9 +159,12 @@ def __generate(value: object, path):
     return value
 
 
-@deprecation.deprecated(deprecated_in="0.1.7", removed_in="0.2.0",
-                        current_version=__version__,
-                        details="Use dataconf.from_file")
+@deprecation.deprecated(
+    deprecated_in="0.1.7",
+    removed_in="0.2.0",
+    current_version=__version__,
+    details="Use dataconf.from_file",
+)
 def load(file: str, clazz):
     try:
         conf = ConfigFactory.parse_file(file)
@@ -172,9 +175,12 @@ def load(file: str, clazz):
         )
 
 
-@deprecation.deprecated(deprecated_in="0.1.7", removed_in="0.2.0",
-                        current_version=__version__,
-                        details="Use dataconf.from_string")
+@deprecation.deprecated(
+    deprecated_in="0.1.7",
+    removed_in="0.2.0",
+    current_version=__version__,
+    details="Use dataconf.from_string",
+)
 def loads(string: str, clazz):
     try:
         conf = ConfigFactory.parse_string(string)
